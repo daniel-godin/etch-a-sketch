0bf183f0c6111d436e7b013a2819090830812247
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2022-11-23 14:48:37 -0800

    All the grids work and with hover too.  Still need to figure out how to dynamically change the square when view-width gets too wide compared to height.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const gridContainer = document.getElementById('gridContainer');
 
-// const divRow = document.getElementsByClassName('grid-item-row');
 
 // Trying Out CSS Grid
 for (let i = 0; i < (16 * 16); i++) {
@@ -12,23 +11,46 @@ for (let i = 0; i < (16 * 16); i++) {
 
 
 
+function hover(element, className) {
+    element.addEventListener('mouseenter', e => element.classList.add(className));
+    // element.addEventListener('mouseleave', e => element.classList.remove(className));
+}
 
 
-// // Loop to create 16 divs.
-// for (let i = 0; i < 16; i++) {
-//     const divRow = document.createElement('div');
-//     gridContainer.appendChild(divRow);
-//     divRow.classList.toggle('grid-item-row');
-//     createCols(i);
+const gridItem = document.getElementsByClassName('grid-item');
 
-// }
+for (let i = 0; i < gridItem.length; i++) {
+    gridItem[i].addEventListener('mouseenter', e => gridItem[i].classList.add('grid-item-hovered'));
 
-// function createCols(rowNum) {
-//     for (let j = 1; j <= 16; j++) {
-//         const divCol = document.createElement('div');
-//         const rowArray = document.querySelectorAll('.grid-item-row');
-//         rowArray[rowNum].appendChild(divCol);
-//         divCol.classList.add('grid-item-column');
-//     }
-    
-// }
+    // hover(gridItem[i], 'grid-item-hovered');
+}
+
+
+function resizeGrid() {
+    let num = prompt("How Many Squares Per Side? (Max: 100)");
+    num = Number(num);
+
+    removeChildNodes(gridContainer);
+
+
+
+
+    for (let i = 0; i < (num * num); i++) {
+        const div = document.createElement('div');
+        gridContainer.appendChild(div);
+        div.classList.add('grid-item');
+        gridContainer.style.cssText = `grid-template-rows: repeat(${num}, minmax(1px, 1fr)); grid-template-columns: repeat(${num}, minmax(1px, 1fr));`
+
+
+    }
+
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener('mouseenter', e => gridItem[i].classList.add('grid-item-hovered'));
+    }
+}
+
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
