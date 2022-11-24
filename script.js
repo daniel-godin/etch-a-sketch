bc14f27002fb4e0c017f8a87369669610ba67113
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2022-11-24 08:31:13 -0800

    Random color on mouseenter works perfectly now.  With both original 16x16 and newly created prompt num by num.  Also, now user cannot enter a number higher than 100.

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
 const gridContainer = document.getElementById('gridContainer');
+const gridItem = document.getElementsByClassName('grid-item');
 
-
-// Trying Out CSS Grid
+// For loop that creates a 16x16 grid until user changes it using resizeGrid() function by clicking on button.
 for (let i = 0; i < (16 * 16); i++) {
     const div = document.createElement('div');
     gridContainer.appendChild(div);
     div.classList.add('grid-item');
 }
 
+// Original for loop for black/white.  New for loop uses randomized colors.
+// for (let i = 0; i < gridItem.length; i++) {
+//     gridItem[i].addEventListener('mouseenter', e => gridItem[i].classList.add('grid-item-hovered')); // Works
+// }
 
-
-
-function hover(element, className) {
-    element.addEventListener('mouseenter', e => element.classList.add(className));
-    // element.addEventListener('mouseleave', e => element.classList.remove(className));
-}
-
-
-const gridItem = document.getElementsByClassName('grid-item');
-
+// Randomizes the color each time user hovers over a box.
 for (let i = 0; i < gridItem.length; i++) {
-    gridItem[i].addEventListener('mouseenter', e => gridItem[i].classList.add('grid-item-hovered'));
-
-    // hover(gridItem[i], 'grid-item-hovered');
-}
-
+    gridItem[i].addEventListener('mouseenter', (event) => {
+        let red = Math.floor(Math.random()*256);
+        let green = Math.floor(Math.random()*256);
+        let blue = Math.floor(Math.random()*256);
+        gridItem[i].style.cssText = `background-color: rgb(${red}, ${green}, ${blue});`
+    });
+};
 
 function resizeGrid() {
-    let num = prompt("How Many Squares Per Side? (Max: 100)");
+    let num;
+    do {
+        num = prompt("How Many Squares Per Side? (Max: 100)");
+    } while (num > 100 && num);
     num = Number(num);
 
-    removeChildNodes(gridContainer);
+    removeChildNodes(gridContainer); // Function to remove all children of gridContainer before adding in a new set of children (divs)
 
-
-
-
+    // for loop that takes user prompted number (num) and creates a CSS grid.
     for (let i = 0; i < (num * num); i++) {
         const div = document.createElement('div');
         gridContainer.appendChild(div);
         div.classList.add('grid-item');
         gridContainer.style.cssText = `grid-template-rows: repeat(${num}, minmax(1px, 1fr)); grid-template-columns: repeat(${num}, minmax(1px, 1fr));`
-
-
     }
+
+    // Black and white version.
+    // for (let i = 0; i < gridItem.length; i++) {
+    //     gridItem[i].addEventListener('mouseenter', e => gridItem[i].classList.add('grid-item-hovered'));
+    // }
 
     for (let i = 0; i < gridItem.length; i++) {
-        gridItem[i].addEventListener('mouseenter', e => gridItem[i].classList.add('grid-item-hovered'));
-    }
+        gridItem[i].addEventListener('mouseenter', (event) => {
+            let red = Math.floor(Math.random()*256);
+            let green = Math.floor(Math.random()*256);
+            let blue = Math.floor(Math.random()*256);
+            gridItem[i].style.cssText = `background-color: rgb(${red}, ${green}, ${blue});`
+        });
+    };
+    
 }
 
 function removeChildNodes(parent) {
